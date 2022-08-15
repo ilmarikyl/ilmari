1467ab66e94b16bef915bd2da10b4e45f5e5da88
ilmarikyl <ilmari_kylliainen@hotmail.com>
2022-08-15 22:47:37 +0300

    add CV link & polish text content

--- src/components/MDXComponents.js
+++ src/components/MDXComponents.js
@@ -22,30 +22,37 @@ const LI = ({ children }) => (
   </li>
 )
 
-const A = ({ href, children }) => (
-  <a
-    href={href}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="text-light-primary-hl hover:cursor-pointer hover:text-red-600 dark:text-yellow-200 dark:hover:text-yellow-300"
-  >
-    {children}
-    {'\u00A0'}
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="inline-block h-[1.2rem] w-[1.2rem]"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth="2"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-      />
-    </svg>
-  </a>
-)
+const A = ({ href, children }) => {
+  const isExternal = href.startsWith('http')
 
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-light-primary-hl hover:cursor-pointer hover:text-red-600 dark:text-yellow-200 dark:hover:text-yellow-300"
+    >
+      {children}
+      {isExternal && (
+        <>
+          {'\u00A0'}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="inline-block h-[1.2rem] w-[1.2rem]"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth="2"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+            />
+          </svg>
+        </>
+      )}
+    </a>
+  )
+}
 export { H1, H2, H3, P, LI, A }
