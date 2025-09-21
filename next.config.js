f44970d5e06f2aa006e4164937323dd2e04cca9c
Ilmari Kylliäinen <ilmari.kylliainen@front.ai>
2025-09-21 19:56:09 +0300

    Remove Babel configuration; add @svgr/webpack

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextTranslate = require('next-translate-plugin')
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   i18n: {
     locales: ['en', 'fi'],
@@ -17,6 +16,14 @@ const nextConfig = {
     // Warning: 'true' allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: false,
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Configure SVGR for SVG imports
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
   },
 }
 
